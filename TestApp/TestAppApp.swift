2f7ade6a11eacf9bceed50c1599f255caf5fab85
Bartosz Klimowicz <klimowicz.bartek@gmail.com>
2022-11-23 20:31:49 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Bartosz Klimowicz on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
